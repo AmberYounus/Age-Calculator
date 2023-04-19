ecf59c24d9921fd36104ddf1de5866f98e6ec606
AmberYounus <65040261+AmberYounus@users.noreply.github.com>
2023-04-20 02:01:56 +0500

    Add files via upload

---
+++ AgeCalculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgeCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime from = dateTimePicker1.Value;
+            DateTime to = DateTime.Now;
+            int age = to.Year - from.Year;
+            if(to.Month>from.Month||to.Month==from.Month||to.Day>from.Month)
+            {
+                textBox1.Text = age.ToString();
+
+            }
+            else{
+                age--; 
+                textBox1.Text = age.ToString();
+
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
